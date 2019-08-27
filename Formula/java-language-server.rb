2b6833fbe72a1538c19e69195663f1f4769b45c8
Duncan Holm <duncan@holm.scot>
2019-08-27 16:29:41 +0100

    Add todo

---
+++ Formula/java-language-server.rb
@@ -0,0 +1,3 @@
+# @todo #0 Write a formula for java-language-server
+#  It has dependencies maven & bazel (available in homebrew-core).
+#  It depends on Java 9+ to build? Since it uses jlink (see clojure formula in homebrew-core for how to specify a depedency on java).
