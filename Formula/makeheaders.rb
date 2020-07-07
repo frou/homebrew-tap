1560b759de2134932bfe664648c19abba47a1822
Duncan Holm <duncan@holm.scot>
2020-07-07 14:43:23 +0100

    Add test for makeheaders

--- Formula/makeheaders.rb
+++ Formula/makeheaders.rb
@@ -1,28 +1,37 @@
 class Makeheaders < Formula
-  desc "Utility to create a tailored .h file for a given .c or .cpp file"
+  desc "Utility to automatically generate a .h file containing declarations for everything a .c file uses"
   homepage "https://www.hwaci.com/sw/mkhdr/"
   url "https://www.fossil-scm.org/home/uv/fossil-src-2.11.1.tar.gz"
   sha256 "b391f34fada5f16eba452f36d2ad8baa3fe2a5267bf8f4169cb1b9832c0cb8eb"
   license "BSD-2-Clause"
 
   def install
-    executable = "makeheaders"
-    system ENV.cc, "src/makeheaders.c", "-o", executable
-    bin.install executable
+    system ENV.cc, "src/#{name}.c", "-o", name
+    bin.install name
   end
 
-  # @todo #0 Implement a test for brew formula Makeheaders
-
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test makeheaders`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    PROVIDER_C = "provider.c"
+    CONSUMER_C = "consumer.c"
+    CONSUMER_H = "consumer.h"
+
+    (testpath/PROVIDER_C).write <<~EOS
+      void something(void) {
+        // ...
+      }
+    EOS
+
+    (testpath/CONSUMER_C).write <<~EOS
+      #include "#{CONSUMER_H}"
+
+      void main(void) {
+          something();
+      }
+    EOS
+
+    system "#{bin}/#{name} *.c"
+    assert_predicate testpath/CONSUMER_H, :exist?
+
+    system "#{ENV.cc} -Werror=implicit-function-declaration *.c"
   end
 end
